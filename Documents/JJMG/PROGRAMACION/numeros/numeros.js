31b73d36b4f0a558777e68aff0f4f4653b334c54
Laboratorio 9 <estudiante309@UNIAJCACM.CO>
2023-09-14 11:15:05 -0500

    se actualizo

--- Documents/JJMG/PROGRAMACION/numeros/numeros.js
+++ Documents/JJMG/PROGRAMACION/numeros/numeros.js
@@ -1,28 +1,30 @@
 var numero;
 var unDijito, dosDijitos, tresDijitos;
 
-numero = parseInt(prompt("ingrese un numero"));
+numero = parseInt(prompt("Ingrese un número"));
 
-switch(numero) {
-    case 1:
-        unDijito = 0>numero<10;
-        unDijito = numero*numero;
-        alert("su resultado es " + unDijito);
+switch (true) {
+
+    case numero > 0 && numero < 10:
+        unDijito = numero * numero;
+        alert("Su resultado es " + unDijito);
         break;
 
-    case 2:
-        dosDijitos = 9>numero<100;
+
+    case numero >= 10 && numero < 100:
         dosDijitos = numero*2;
-        alert("su resultado es " + dosDijitos);
+        alert("Su resultado es " + dosDijitos);
         break;
+ 
 
-    case 3:
-        tresDijitos = 99>numero<1000;
-        tresDijitos = numero-100;
-        alert("su respuesta es " + tresDijitos);
+    case numero >= 100 && numero < 1000:
+        tresDijitos = numero -100;
+        alert("Su respuesta es " + tresDijitos);
         break;
+ 
 
     default:
-        alert("numero no valido");
+        alert("Número no válido");
         break;
+
 }
